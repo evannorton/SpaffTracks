e4c7993aac0cf0e18fe7e12598ff2e88f6a0c731
dstapleton92 <david@platypi.io>
2018-02-01 15:31:57 -0600

    removed emit-all-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const EmitAllPlugin = require('webpack-emit-all-plugin');
 
 const CLIENT_PATH = path.join(__dirname, './client/dist');
 const SERVER_PATH = path.join(__dirname, './server/lib');
