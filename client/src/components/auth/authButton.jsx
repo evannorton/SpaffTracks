ac6ef8693eace1c597b1ced075388f159c716297
William Johnston <johnston.w@gmail.com>
2018-02-13 15:44:19 -0600

    removing withRouter from AuthButton

--- client/src/components/auth/authButton.jsx
+++ client/src/components/auth/authButton.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { withRouter, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { isLoggedIn } from '../../services/user';
 
-const AuthButton = withRouter(
-    ({ history }) => {
-        if (isLoggedIn()) {
-            return <Link className="btn btn-info" to="/logout">Logout</Link>;
-        } else {
-            return <Link className="btn btn-info" to="/login">Login</Link>;
-        }
+const AuthButton = (props) => {
+    if (isLoggedIn()) {
+        return <Link className="btn btn-info" to="/logout">Logout</Link>;
+    } else {
+        return <Link className="btn btn-info" to="/login">Login</Link>;
     }
-);
+};
 
 export default AuthButton;
