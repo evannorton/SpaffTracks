40e7c9a739dbef77135762c30010adbfe1baaf9f
Evan Norton <evanan39@yahoo.com>
2018-03-21 04:43:27 -0500

    styling

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -7,7 +7,7 @@ export default class Header extends Component {
             <header className="container-fluid">
                 <div className="row">
                     <div className="col-12 d-flex align-items-center justify-content-center">
-                        <Link to="/">
+                        <Link to="/" id="logo-link">
                             <img id="logo" src="/images/SpaffTracks.png" alt="SpaffTracks" />
                         </Link>
                     </div>
