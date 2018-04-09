b4a566dcf4df95f99ccaab92711f965c11b55761
Evan Norton <evanan39@yahoo.com>
2018-04-09 07:05:33 -0500

    show duration of tracks

--- client/src/components/show.jsx
+++ client/src/components/show.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default class Show extends Component {
     render() {
         return (
-            <Link to={`/show/${this.props.show.date}`} className="col-12 show bubble-tile d-flex align-items-center justify-content-center">
+            <Link to={`/show/${this.props.show.date}`} className="col-12 show bubble-tile d-flex align-items-center justify-content-start">
                 {this.props.show.date} - {this.props.show.venue}, {this.props.show.city}
             </Link>
         );
--- client/src/components/track.jsx
+++ client/src/components/track.jsx
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Show extends Component {
+
+    componentDidMount() {
+        let audio = $("#audio-" + this.props.track.id)[0];
+        let duration = $("#duration-" + this.props.track.id);
+        if (audio) {
+            audio.ondurationchange = () => {
+                let durationMinutes = Math.floor(audio.duration / 60).toString();
+                let durationSeconds = Math.floor(audio.duration % 60).toString();
+                if (durationSeconds.length === 1) {
+                    durationSeconds = "0" + durationSeconds;
+                }
+                duration.text(durationMinutes + ":" + durationSeconds);
+            };
+        }
+    }
+
     render() {
         return (
             <div
-                className="col-12 track bubble-tile d-flex align-items-center justify-content-center"
+                className="col-12 track bubble-tile d-flex align-items-center justify-content-left"
                 onClick={() => { this.props.setClickedTrack(this.props.track) }}
             >
-                {this.props.track.title}
+                <audio id={"audio-" + this.props.track.id} src={this.props.track.url} type="audio/mp3"></audio>
+                <p>
+                    {this.props.track.title}
+                </p>
+
+                <span className="duration" id={"duration-" + this.props.track.id}>
+                </span>
             </div>
         );
     }
+
 }
